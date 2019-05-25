08318c769dc0e229af916f01a54b8d6e39820033
Hector Zurga <hjzurga@oxoftmedia.com>
2019-05-25 14:55:37 -0400

    Video 8: Configuracion de base de datos

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
-const { buildSchema } = require('graphql')
+require('dotenv').config()
+const { makeExecutableSchema } = require('graphql-tools')
 const express = require('express')
 const gqlMiddleware = require('express-graphql')
 const { readFileSync } = require('fs')
@@ -11,12 +12,11 @@ const app = express()
 const port = process.env.port || 3000
 
 // definiendo el schema
-const schema = buildSchema(
-  readFileSync(
-    join(__dirname, 'lib', 'schema.graphql'),
-    'utf-8'
-  )
+const typeDefs = readFileSync(
+  join(__dirname, 'lib', 'schema.graphql'),
+  'utf-8'
 )
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 app.use('/api', gqlMiddleware({
   schema: schema,
