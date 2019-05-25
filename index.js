0f8278323a8af300b75ae75b153ee5fef5a8320c
Hector Zurga <hjzurga@oxoftmedia.com>
2019-05-25 09:34:01 -0400

    Video 5: Sirviendo el API en la web

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 'use strict'
 
-const { graphql, buildSchema } = require('graphql');
+const { graphql, buildSchema } = require('graphql')
+const express = require('express')
+const gqlMiddleware = require('express-graphql')
+
+
+const app = express()
+const port = process.env.port || 3000
 
 // definiendo el schema
 const schema = buildSchema(`
   type Query {
     hello: String
-    saludo: String
   }
 `)
 // tipos escalares String, Integer, Float & Boolean
@@ -15,13 +20,15 @@ const schema = buildSchema(`
 const resolvers = {
   hello: () => {
     return 'Hello World'
-  },
-  saludo: () => {
-    return 'Hola a todos'
   }
 }
 
-// Ejecutar el query hello
-graphql(schema, '{ saludo }', resolvers).then(data => {
-  console.log(data)
+app.use('/api', gqlMiddleware({
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
+}))
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}/api`)
 })
