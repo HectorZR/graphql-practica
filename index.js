3d32af89185315ee97621064a4382cffa3d71980
Hector Zurga <hjzurga@oxoftmedia.com>
2019-05-25 10:32:42 -0400

    Video 6: Custom Types

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
 'use strict'
 
-const { graphql, buildSchema } = require('graphql')
+const { buildSchema } = require('graphql')
 const express = require('express')
 const gqlMiddleware = require('express-graphql')
-
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const resolvers = require('./lib/resolvers.js')
 
 const app = express()
 const port = process.env.port || 3000
 
 // definiendo el schema
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`)
-// tipos escalares String, Integer, Float & Boolean
-
-// Configura los resolvers
-const resolvers = {
-  hello: () => {
-    return 'Hello World'
-  }
-}
+const schema = buildSchema(
+  readFileSync(
+    join(__dirname, 'lib', 'schema.graphql'),
+    'utf-8'
+  )
+)
 
 app.use('/api', gqlMiddleware({
   schema: schema,
